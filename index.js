77f5a9b6a3a7b01f493b54bf2509d138aa112ae7
Regis <boudinot.regis@yahoo.com>
2016-06-17 19:29:52 -0600

    single for better flexibility

--- index.js
+++ index.js
@@ -8,32 +8,16 @@ class Lspi {
     }
   }
 
-  createRecords() {
-    Array.from(arguments).map(record => this.createRecord(record))
-  }
-
   updateRecord(recordName, data) {
     localStorage.setItem(recordName, JSON.stringify(data))
-  }
-
-  updateRecords() {
-    return Array.from(arguments).map(arg => { this.updateRecord(arg[0], arg[1]) })
   }
 
   getRecord(recordName) {
     return JSON.parse(localStorage.getItem(recordName))
   }
 
-  getRecords() {
-    return Array.from(arguments).map(record => { this.getRecord(record) })
-  }
-
   deleteRecord(recordName) {
     localStorage.clear(recordName)
-  }
-
-  deleteRecords() {
-    return Array.from(arguments).map(record => { this.deleteRecord(record) })
   }
 }
 
