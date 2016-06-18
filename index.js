a2f7e00586c1fba2fad1b3b406f14dbc97d4ae11
Regis <boudinot.regis@yahoo.com>
2016-06-17 18:23:54 -0600

    MVP for LSPI

---
+++ index.js
@@ -0,0 +1,40 @@
+class Lspi {
+  createRecord(recordName) {
+    const recordCheck = this.getRecord(recordName)
+    if (recordCheck === undefined) { 
+      return console.log(`${recordName} already exists!`) 
+    } else if (recordCheck) {
+      localStorage.setItem(recordName, JSON.stringify({}))
+    }
+  }
+
+  createRecords() {
+    Array.from(arguments).map(record => this.createRecord(record))
+  }
+
+  updateRecord(recordName, data) {
+    localStorage.setItem(recordName, JSON.stringify(data))
+  }
+
+  updateRecords() {
+    return Array.from(arguments).map(arg => { this.updateRecord(arg[0], arg[1]) })
+  }
+
+  getRecord(recordName) {
+    return JSON.parse(localStorage.getItem(recordName))
+  }
+
+  getRecords() {
+    return Array.from(arguments).map(record => { this.getRecord(record) })
+  }
+
+  deleteRecord(recordName) {
+    localStorage.clear(recordName)
+  }
+
+  deleteRecords() {
+    return Array.from(arguments).map(record => { this.deleteRecord(record) })
+  }
+}
+
+const lspi = new Lspi
