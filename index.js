8be17dd40ea6473af12fbd6eeb9a12bbb4d47ce3
selfup <boudinot.regis@yahoo.com>
2016-12-11 12:45:40 -0700

    add Object.assign polyfill

--- index.js
+++ index.js
@@ -15,6 +15,37 @@ var _jsonOrString = function _jsonOrString(data) {
 var Lspi = function () {
   function Lspi() {
     _classCallCheck(this, Lspi);
+
+    // MDN polyfill for Object.assign
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+    if (typeof Object.assign != 'function') {
+      Object.assign = function (target, varArgs) {
+        // .length of function is 2
+        'use strict';
+
+        if (target == null) {
+          // TypeError if undefined or null
+          throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        var to = Object(target);
+
+        for (var index = 1; index < arguments.length; index++) {
+          var nextSource = arguments[index];
+
+          if (nextSource != null) {
+            // Skip over if undefined or null
+            for (var nextKey in nextSource) {
+              // Avoid bugs when hasOwnProperty is shadowed
+              if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                to[nextKey] = nextSource[nextKey];
+              }
+            }
+          }
+        }
+        return to;
+      };
+    }
   }
 
   _createClass(Lspi, [{
